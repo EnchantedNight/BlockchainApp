3466114979bba645ed50d7191e447d098dfd3b53
EnchantedNight <neffex.god9@gmail.com>
2026-02-09 14:45:08 +0000

    disable ton analytics

--- apps/client/src/tonClient.ts
+++ apps/client/src/tonClient.ts
@@ -2,4 +2,7 @@ import { TonConnectUI } from "@tonconnect/ui";
 
 export default new TonConnectUI({
   manifestUrl: "http://localhost:5173/tonconnect-manifest.json",
+  analytics: {
+    mode: "off",
+  },
 });
