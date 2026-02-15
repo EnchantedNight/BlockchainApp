74464d1435eb1e728d483196422af7a48bdf0715
EnchantedNight <neffex.god9@gmail.com>
2026-02-15 20:40:21 +0000

    add vite plugin for buffer

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), nodePolyfills()],
   server: {
     headers: {
       "Access-Control-Allow-Origin": "*",
