46b5b981a19ab7c3da2c9fca704b98dd5cb2086b
EnchantedNight <neffex.god9@gmail.com>
2026-02-19 16:14:01 +0000

    deploy 1771517641

--- apps/client/playwright.config.ts
+++ apps/client/playwright.config.ts
@@ -9,6 +9,12 @@ export default defineConfig({
     baseURL: process.env.BASE_URL || "http://localhost:5173",
     headless: false,
     actionTimeout: 120000,
+    extraHTTPHeaders: process.env.VERCEL_AUTOMATION_BYPASS_SECRET,
+          ? {
+              "x-vercel-protection-bypass":
+                process.env.VERCEL_AUTOMATION_BYPASS_SECRET,
+            }
+          : {},
   },
   webServer: process.env.BASE_URL
     ? undefined
