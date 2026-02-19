ca405831ea35380d157f97977ac4a0636eb6b510
EnchantedNight <neffex.god9@gmail.com>
2026-02-19 11:06:46 +0000

    set tests dir to test/

--- apps/client/playwright.config.ts
+++ apps/client/playwright.config.ts
@@ -2,6 +2,8 @@ import "dotenv/config";
 import { defineConfig, devices } from "@playwright/test";
 
 export default defineConfig({
+  testDir: "./test",
+  outputDir: "/tmp/playwright",
   use: {
     baseURL: process.env.BASE_URL || "http://localhost:5173",
     headless: !!process.env.CI,
